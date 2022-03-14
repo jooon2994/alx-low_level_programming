f7b5898cea327e77cd0095db43f0b1d17c8aa800
jooon2994 <wizyoni7@gmail.com>
2022-03-14 21:53:28 +0300

    new

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+
+/**
+ * main - Entry point of our program
+ * This is the main function. where everything starts.
+ *
+ * Return: Always return 0 (success)
+ */
+int main(void)
+{
+puts("programing is  like building a multilingual puzzle");
+return (0);
+}
