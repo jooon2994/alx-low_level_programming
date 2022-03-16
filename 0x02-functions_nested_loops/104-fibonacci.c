b2560f51c79df7c3c00d6d8bd5cbb10c4bba0b88
jooon2994 <99585572+jooon2994@users.noreply.github.com>
2022-03-16 17:53:17 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,31 +1,41 @@
-#include <stdio.h>
+#include<stdio.h>
+
 /**
- * main - main function
+ * main - first 50 fibonatchi.
  *
- * Return: nothing
+ * Return: void.
  */
 int main(void)
 {
-	int counter = 2;
+	unsigned long int i, i0, i1, j, j0, j1, e, e0, e1, y, k;
 
-	float a = 1;
-	float b = a + 1;
-	float c = a + b;
+	i = 1;
+	j = 2;
+	e = i + j;
 
-	printf("%.0f, ", a);
-	printf("%.0f, ", b);
-	while (counter < 98)
+	printf("%lu, ", i);
+	printf("%lu, ", j);
+	for (y = 3; y < 89; y++)
 	{
-		counter++;
-		printf("%.0f", c);
-		a = b;
-		b = c;
-		c = a + b;
-		if (counter < 98)
-		{
-			printf(", ");
-		}
+		printf("%lu, ", e);
+		i = j;
+		j = e;
+		e = i + j;
 	}
-	printf("\n");
+	j0 = j / 1000000000;
+	j1 = j % 1000000000;
+	e0 = e / 1000000000;
+	e1 = e % 1000000000;
+	for (k = 89; k < 98; k++)
+	{
+		printf("%lu%lu, ", e0, e1);
+		i0 = j0;
+		i1 = j1;
+		j0 = e0;
+		j1 = e1;
+		e0 = i0 + j0 + ((i1 + j1) / 1000000000);
+		e1 = (i1 + j1) % 1000000000;
+	}
+printf("%lu%lu\n", e0, e1);
 	return (0);
 }
